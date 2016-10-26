806d7fcdda88f3098ef170d1ef72cb477d799d95
Sahba Bahizad <sarahjune919@gmail.com>
2016-10-26 16:14:16 -0700

    renamed the package to helper

---
+++ Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/helper/StringParsing.java
@@ -0,0 +1,14 @@
+package edu.greenriver.it.helper;
+
+public final class StringParsing {
+
+	public static String before(String subject, String delimiter)
+	{
+		return null;
+	}
+	
+	public static String after(String subject, String delimiter)
+	{
+		return null;
+	}
+}
---
+++ Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/helper/StringRegex.java
@@ -0,0 +1,25 @@
+package edu.greenriver.it.helper;
+
+public final class StringRegex {
+
+	public static boolean isEmail(String subject) {
+		return false;
+
+	}
+
+	public static boolean isZipCode(String subject) {
+		return false;
+
+	}
+
+	public static boolean isPhoneNumber(String subject) {
+		return false;
+
+	}
+
+	public static boolean isUrl(String subject){
+		return false;
+		
+	}
+
+}
