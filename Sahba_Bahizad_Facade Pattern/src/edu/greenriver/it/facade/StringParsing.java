3855b0f58fc913f1c62fda0e470e0ebd638ccee2
Sahba Bahizad <sarahjune919@gmail.com>
2016-10-26 16:13:40 -0700

    Part 1 Class Definitions

---
+++ Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/facade/StringParsing.java
@@ -0,0 +1,14 @@
+package edu.greenriver.it.facade;
+
+public final class StringParsing {
+
+	public static String before(String subject, String delimiter)
+	{
+		return null;
+	}
+	
+	public static String after(String subject, String delimiter)
+	{
+		return null;
+	}
+}
