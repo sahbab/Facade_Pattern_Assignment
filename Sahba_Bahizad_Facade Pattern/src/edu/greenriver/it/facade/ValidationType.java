4c1b6e9feaa8cbcc7e819e7f5f0a8fe3f3c43584
Sahba Bahizad <sarahjune919@gmail.com>
2016-10-27 11:59:35 -0700

    Tested Regex and Added Facade Package

---
+++ Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/facade/ValidationType.java
@@ -0,0 +1,6 @@
+package edu.greenriver.it.facade;
+
+public enum ValidationType {
+
+	 EMAIL, ZIPCODE, PHONE_NUMBER , URL
+}
--- Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/helper/StringRegex.java
+++ Sahba_Bahizad_Facade Pattern/src/edu/greenriver/it/helper/StringRegex.java
@@ -1,5 +1,8 @@
 package edu.greenriver.it.helper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class StringRegex {
 
 	/**
@@ -8,27 +11,39 @@ public final class StringRegex {
 	 * @return Returns true if the subject is a valid email address
 	 */
 	public static boolean isEmail(String subject) {
-		return false;
+
+		final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(subject);
+		return matcher.find();
 
 	}
 
 	/**
 	 * 
 	 * @param subject
-	 * @return Returns true if the subject is either of the following formats: 88888 or 88888-7777
+	 * @return Returns true if the subject is either of the following formats:
+	 *         88888 or 88888-7777
 	 */
 	public static boolean isZipCode(String subject) {
-		return false;
+		final Pattern VALID_ZIP_CODE_REGEX = Pattern.compile("^\\d{5}(-\\d{4})?$",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = VALID_ZIP_CODE_REGEX.matcher(subject);
+		return matcher.find();
 
 	}
 
 	/**
 	 * 
 	 * @param subject
-	 * @return Returns true if the subject is either of the following formats: 999-888-7777 or 9998887777
+	 * @return Returns true if the subject is either of the following formats:
+	 *         999-888-7777 or 9998887777
 	 */
 	public static boolean isPhoneNumber(String subject) {
-		return false;
+		final Pattern VALID_ZIP_CODE_REGEX = Pattern.compile("^(\\d{3}-?\\d{3}-?\\d{4})$",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = VALID_ZIP_CODE_REGEX.matcher(subject);
+		return matcher.find();
 
 	}
 
@@ -37,9 +52,12 @@ public final class StringRegex {
 	 * @param subject
 	 * @return Returns true if the subject is a valid url
 	 */
-	public static boolean isUrl(String subject){
-		return false;
-		
+	public static boolean isUrl(String subject) {
+		final Pattern VALID_ZIP_CODE_REGEX = Pattern.compile("^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$",
+				Pattern.CASE_INSENSITIVE);
+		Matcher matcher = VALID_ZIP_CODE_REGEX.matcher(subject);
+		return matcher.find();
+
 	}
 
 }
